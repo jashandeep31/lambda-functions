1f95f00bd04d730daadf6df28f72f9ddc1e813e1
Jashandeep Singh <jashandeep1659@gmail.com>
2025-08-14 18:03:41 +0530

    feat: endpoint support to enable cloudflare r2

--- download-media-and-upload-to-s3/index.js
+++ download-media-and-upload-to-s3/index.js
@@ -5,6 +5,7 @@ import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
 const client = new S3Client({
   region: process.env.S3_REGION,
+  ...(process.env.S3_ENDPOINT && { endpoint: process.env.S3_ENDPOINT }),
   credentials: {
     accessKeyId: process.env.S3_BUCKET_ACCESS_KEY_ID,
     secretAccessKey: process.env.S3_BUCKET_SECRET_ACCESS_KEY,
